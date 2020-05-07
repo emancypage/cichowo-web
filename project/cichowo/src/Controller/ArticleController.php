0190aecd30c7cba711143bc15c8d382aa7fdb867
s_szymkowiak2 <szymon.szymkowiak@kiratik.de>
2020-05-07 21:21:54 +0200

    Init files

--- project/cichowo/src/Controller/ArticleController.php
+++ project/cichowo/src/Controller/ArticleController.php
@@ -5,27 +5,31 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return $this->render('base.html.twig');
+        return $this->render('homepage.html.twig');
     }
 
     /**
-     * @Route("/articles/{slug}")
+     * @Route("/articles/{slug}", name="single_art")
      */
     public function article($slug)
     {
-        return $this->render('articles/template.html.twig', [
-            'title' => $slug
+        return $this->render('articles/article.html.twig', [
+            'title' => $slug,
+            'comments' => [
+                'comment1',
+                'comment2',
+                'comment3'
+            ]
         ]);
     }
 }
